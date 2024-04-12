ea4852ecedde279e9549ead538d9778ee962e63a
Sean Ross <sean.r.ross@gmail.com>
2024-04-12 17:50:40 -0600

    randommac via udev

--- modules/nanopi/randommac.nix
+++ modules/nanopi/randommac.nix
@@ -1,12 +1,14 @@
 { config, pkgs, lib, modulesPath, ... }: {
-  config.boot.postBootCommands = ''
+  config.boot.postBootCommands = lib.mkBefore ''
     ${config.boot.postBootCommands}
 
-    if [[ ! -e /var/lib/private/lan_mac ]]; 
-      printf '00:60:2F:%02X:%02X:%02X\n' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256] > /var/lib/private/lan_mac
-    fi
-    if [[ ! -e /var/lib/private/wan_mac ]];
-      printf '00:60:2F:%02X:%02X:%02X\n' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256] > /var/lib/private/wan_mac
+    random_mac() {
+      printf '00:60:2F:%02X:%02X:%02X\n' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256]'
+    }
+
+    if [[ ! -e /etc/udev/rules.d/10-network-persistent-custom-mac-address.rules ]]; 
+      echo "SUBSYSTEM==\"net\", ACTION==\"add\", KERNEL==\"eth0\", ATTR{address}==\"$(random_mac)\"" >> /etc/udev/rules.d/10-network-persistent-custom-mac-address.rules
+      echo "SUBSYSTEM==\"net\", ACTION==\"add\", KERNEL==\"eth1\", ATTR{address}==\"$(random_mac)\"" >> /etc/udev/rules.d/10-network-persistent-custom-mac-address.rules
     fi
   '';
 }
